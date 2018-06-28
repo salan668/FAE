1a252f15a5566aa39ded7e9b8e676017ad5c1f8b
DESKTOP-4V8BJL8\SY <songyangmri@gmail.com>
2018-06-28 15:10:02 +0800

    Update ReliefF based on the module ReliefF. This file was added get_support function and get_score function. Add saving the scores of the selected features.

--- FAE/FeatureAnalysis/FeatureSelector.py
+++ FAE/FeatureAnalysis/FeatureSelector.py
@@ -11,6 +11,7 @@ from sklearn.feature_selection import SelectKBest, f_classif, RFE
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
+from FAE.FeatureAnalysis.ReliefF import ReliefF
 from FAE.DataContainer.DataContainer import DataContainer
 
 
@@ -281,13 +282,15 @@ class FeatureSelectByANOVA(FeatureSelectByAnalysis):
         fs = SelectKBest(f_classif, k=self.GetSelectedFeatureNumber())
         fs.fit(data, label)
         feature_index = fs.get_support(True)
-        return feature_index.tolist()
+        f_value, p_value = f_classif(data, label)
+        return feature_index.tolist(), f_value, p_value
 
     def GetName(self):
         return 'ANOVA'
 
     def Run(self, data_container, store_folder=''):
-        new_data_container = self.SelectFeatureByIndex(data_container, self.GetSelectedFeatureIndex(data_container), is_replace=False)
+        selected_index, f_value, p_value = self.GetSelectedFeatureIndex(data_container)
+        new_data_container = self.SelectFeatureByIndex(data_container, selected_index, is_replace=False)
         if store_folder and os.path.isdir(store_folder):
             feature_store_path = os.path.join(store_folder, 'selected_feature.csv')
             featureinfo_store_path = os.path.join(store_folder, 'feature_select_info.csv')
@@ -295,129 +298,50 @@ class FeatureSelectByANOVA(FeatureSelectByAnalysis):
             new_data_container.Save(feature_store_path)
             SaveSelectInfo(new_data_container, featureinfo_store_path, is_merge=False)
 
+            anova_sort_path = os.path.join(store_folder, 'anova_sort.csv')
+            df = pd.DataFrame(data=np.stack((f_value, p_value), axis=1), index=data_container.GetFeatureName(), columns=['F', 'P'])
+            df.to_csv(anova_sort_path)
+
         return new_data_container
 
 class FeatureSelectByRelief(FeatureSelectByAnalysis):
-    def __init__(self, selected_feature_number=1, iter_ratio=400):
+    def __init__(self, selected_feature_number=1, iter_ratio=0.6):
         super(FeatureSelectByRelief, self).__init__(selected_feature_number)
         self.__iter_radio = iter_ratio
 
-    def __SortByValue(self, feature_score):
-        feature_list = []
-        sorted_feature_number_list = []
-        for feature_index in range(len(feature_score)):
-            feature_list_unit = []
-            feature_list_unit.append(feature_score[feature_index])
-            feature_list_unit.append(feature_index)
-            feature_list.append(feature_list_unit)
-        sorted_feature_list = sorted(feature_list, key=lambda x: x[0], reverse=True)
-        for feature_index in range(len(sorted_feature_list)):
-            sorted_feature_number_list.append(sorted_feature_list[feature_index][1])
-
-        return sorted_feature_number_list
-
-    def __DistanceNorm(self, Norm, D_value):
-        # initialization
-
-        # Norm for distance
-        if Norm == '1':
-            counter = np.absolute(D_value)
-            counter = np.sum(counter)
-        elif Norm == '2':
-            counter = np.power(D_value, 2)
-            counter = np.sum(counter)
-            counter = np.sqrt(counter)
-        elif Norm == 'Infinity':
-            counter = np.absolute(D_value)
-            counter = np.max(counter)
-        else:
-            raise Exception('We will program this later......')
-
-        return counter
-
-    def __SortByRelief(self, data_container):
+    def GetSelectedFeatureIndex(self, data_container):
         data = data_container.GetArray()
         data /= np.linalg.norm(data, ord=2, axis=0)
         label = data_container.GetLabel()
 
-        # initialization
-        (n_samples, n_features) = np.shape(data)
-        distance = np.zeros((n_samples, n_samples))
-        weight = np.zeros(n_features)
-
-        if self.__iter_radio >= 0.5:
-            # compute distance
-            for index_i in range(n_samples):
-                for index_j in range(index_i + 1, n_samples):
-                    D_value = data[index_i] - data[index_j]
-                    distance[index_i, index_j] = self.__DistanceNorm('2', D_value)
-            distance += distance.T
-        else:
-            pass
-
-            # start iteration
-        for iter_num in range(int(self.__iter_radio * n_samples)):
-            # print iter_num;
-            # initialization
-            nearHit = list()
-            nearMiss = list()
-            distance_sort = list()
-
-            # random extract a sample
-            index_i = randrange(0, n_samples, 1)
-            self_features = data[index_i]
-
-            # search for nearHit and nearMiss
-            if self.__iter_radio >= 0.5:
-                distance[index_i, index_i] = np.max(distance[index_i])  # filter self-distance
-                for index in range(n_samples):
-                    distance_sort.append([distance[index_i, index], index, label[index]])
-            else:
-                # compute distance respectively
-                distance = np.zeros(n_samples)
-                for index_j in range(n_samples):
-                    D_value = data[index_i] - data[index_j]
-                    distance[index_j] = self.__DistanceNorm('2', D_value)
-                distance[index_i] = np.max(distance)  # filter self-distance
-                for index in range(n_samples):
-                    distance_sort.append([distance[index], index, label[index]])
-            distance_sort.sort(key=lambda x: x[0])
-            for index in range(n_samples):
-                if nearHit == [] and distance_sort[index][2] == label[index_i]:
-                    # nearHit = distance_sort[index][1];
-                    nearHit = data[distance_sort[index][1]]
-                elif nearMiss == [] and distance_sort[index][2] != label[index_i]:
-                    # nearMiss = distance_sort[index][1]
-                    nearMiss = data[distance_sort[index][1]]
-                elif nearHit != [] and nearMiss != []:
-                    break
-                else:
-                    continue
-
-                    # update weight
-            weight = weight - np.power(self_features - nearHit, 2) + np.power(self_features - nearMiss, 2)
-
-        return self.__SortByValue(weight / (self.__iter_radio * n_samples))
-
-    def GetSelectedFeatureIndex(self, data_container):
-        feature_sort_list = self.__SortByRelief(data_container)
-        if len(feature_sort_list) < self.GetSelectedFeatureNumber():
+        if data.shape[1] < self.GetSelectedFeatureNumber():
             print('The number of features in data container is smaller than the required number')
-            self.SetSelectedFeatureNumber(len(feature_sort_list))
-        selected_feature_index = feature_sort_list[:self.GetSelectedFeatureNumber()]
-        return selected_feature_index
+            self.SetSelectedFeatureNumber(data.shape[1])
+
+        relief = ReliefF(n_features_to_keep=self.GetSelectedFeatureNumber())
+        relief.fit(data, label)
+        selected_feature_index = relief.get_support()
+        score = relief.get_score()
+
+        return selected_feature_index.tolist(), score
 
     def GetName(self):
         return 'Relief'
 
     def Run(self, data_container, store_folder=''):
-        new_data_container = self.SelectFeatureByIndex(data_container, self.GetSelectedFeatureIndex(data_container), is_replace=False)
+        selected_index, score = self.GetSelectedFeatureIndex(data_container)
+        new_data_container = self.SelectFeatureByIndex(data_container, selected_index, is_replace=False)
         if store_folder and os.path.isdir(store_folder):
             feature_store_path = os.path.join(store_folder, 'selected_feature.csv')
             featureinfo_store_path = os.path.join(store_folder, 'feature_select_info.csv')
 
             new_data_container.Save(feature_store_path)
             SaveSelectInfo(new_data_container, featureinfo_store_path, is_merge=False)
+
+            relief_sort_path = os.path.join(store_folder, 'relief_sort.csv')
+            df = pd.DataFrame(data=score, index=data_container.GetFeatureName(), columns=['Scores'])
+            df.to_csv(relief_sort_path)
+
         return new_data_container
 
 class FeatureSelectByRFE(FeatureSelectByAnalysis):
@@ -437,20 +361,26 @@ class FeatureSelectByRFE(FeatureSelectByAnalysis):
         fs = RFE(self.__classifier, self.GetSelectedFeatureNumber(), step=0.05)
         fs.fit(data, label)
         feature_index = fs.get_support(True)
+        ranks = fs.ranking_
 
-        return feature_index.tolist()
+        return feature_index.tolist(), ranks
 
     def GetName(self):
         return 'RFE'
 
     def Run(self, data_container, store_folder=''):
-        new_data_container = self.SelectFeatureByIndex(data_container, self.GetSelectedFeatureIndex(data_container), is_replace=False)
+        selected_index, rank = self.GetSelectedFeatureIndex(data_container)
+        new_data_container = self.SelectFeatureByIndex(data_container, selected_index, is_replace=False)
         if store_folder and os.path.isdir(store_folder):
             feature_store_path = os.path.join(store_folder, 'selected_feature.csv')
             featureinfo_store_path = os.path.join(store_folder, 'feature_select_info.csv')
 
             new_data_container.Save(feature_store_path)
             SaveSelectInfo(new_data_container, featureinfo_store_path, is_merge=False)
+
+            rfe_sort_path = os.path.join(store_folder, 'RFE_sort.csv')
+            df = pd.DataFrame(data=rank, index=data_container.GetFeatureName(), columns=['rank'])
+            df.to_csv(rfe_sort_path)
 
         return new_data_container
 
---
+++ FAE/FeatureAnalysis/ReliefF.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2016 Randal S. Olson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from __future__ import print_function
+import numpy as np
+from sklearn.neighbors import KDTree
+
+
+class ReliefF(object):
+
+    """Feature selection using data-mined expert knowledge.
+
+    Based on the ReliefF algorithm as introduced in:
+
+    Kononenko, Igor et al. Overcoming the myopia of inductive learning
+    algorithms with RELIEFF (1997), Applied Intelligence, 7(1), p39-55
+
+    """
+
+    def __init__(self, n_neighbors=100, n_features_to_keep=10):
+        """Sets up ReliefF to perform feature selection.
+
+        Parameters
+        ----------
+        n_neighbors: int (default: 100)
+            The number of neighbors to consider when assigning feature
+            importance scores.
+            More neighbors results in more accurate scores, but takes longer.
+
+        Returns
+        -------
+        None
+
+        """
+
+        self.feature_scores = None
+        self.top_features = None
+        self.tree = None
+        self.n_neighbors = n_neighbors
+        self.n_features_to_keep = n_features_to_keep
+
+    def fit(self, X, y):
+        """Computes the feature importance scores from the training data.
+
+        Parameters
+        ----------
+        X: array-like {n_samples, n_features}
+            Training instances to compute the feature importance scores from
+        y: array-like {n_samples}
+            Training labels
+        }
+
+        Returns
+        -------
+        None
+
+        """
+        self.feature_scores = np.zeros(X.shape[1])
+        self.tree = KDTree(X)
+
+        for source_index in range(X.shape[0]):
+            distances, indices = self.tree.query(
+                X[source_index].reshape(1, -1), k=self.n_neighbors+1)
+
+            # Nearest neighbor is self, so ignore first match
+            indices = indices[0][1:]
+
+            # Create a binary array that is 1 when the source and neighbor
+            #  match and -1 everywhere else, for labels and features..
+            labels_match = np.equal(y[source_index], y[indices]) * 2. - 1.
+            features_match = np.equal(X[source_index], X[indices]) * 2. - 1.
+
+            # The change in feature_scores is the dot product of these  arrays
+            self.feature_scores += np.dot(features_match.T, labels_match)
+
+        self.top_features = np.argsort(self.feature_scores)[::-1]
+
+    def transform(self, X):
+        """Reduces the feature set down to the top `n_features_to_keep` features.
+
+        Parameters
+        ----------
+        X: array-like {n_samples, n_features}
+            Feature matrix to perform feature selection on
+
+        Returns
+        -------
+        X_reduced: array-like {n_samples, n_features_to_keep}
+            Reduced feature matrix
+
+        """
+        return X[:, self.top_features[:self.n_features_to_keep]]
+
+    def fit_transform(self, X, y):
+        """Computes the feature importance scores from the training data, then
+        reduces the feature set down to the top `n_features_to_keep` features.
+
+        Parameters
+        ----------
+        X: array-like {n_samples, n_features}
+            Training instances to compute the feature importance scores from
+        y: array-like {n_samples}
+            Training labels
+
+        Returns
+        -------
+        X_reduced: array-like {n_samples, n_features_to_keep}
+            Reduced feature matrix
+
+        """
+        self.fit(X, y)
+        return self.transform(X)
+
+    def get_support(self):
+        return self.top_features[:self.n_features_to_keep]
+
+    def get_score(self):
+        return self.feature_scores
