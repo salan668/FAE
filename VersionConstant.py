164c3e67005c339ee9401694fc42b5dbb51353db
Yang Song <songyangmri@gmail.com>
2022-01-25 16:06:49 +0800

    Release 0.0.5

--- VersionConstant.py
+++ VersionConstant.py
@@ -6,9 +6,8 @@ All rights reserved.
 
 # Version Key
 MAJOR = 0
-MINOR = 4
-PATCH = 4
+MINOR = 5
+PATCH = 0
 VERSION_NAME = 'Version'
 VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
-ACCEPT_VERSION = ['0.3.0', '0.3.1', '0.3.2', '0.3.3', '0.3.4', '0.3.5', '0.3.6', '0.3.7', '0.4.0', '0.4.1', '0.4.2',
-                  '0.4.3', '0.4.4']
+ACCEPT_VERSION = ['0.5.0']
