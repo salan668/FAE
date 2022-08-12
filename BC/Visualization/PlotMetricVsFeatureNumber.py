7e48e0fb321dff940ac793e48011a167d4e52b64
zhangjingcode <798582238@qq.com>
2022-08-13 00:00:18 +0800

    Rename "Val"

--- BC/Visualization/PlotMetricVsFeatureNumber.py
+++ BC/Visualization/PlotMetricVsFeatureNumber.py
@@ -46,7 +46,7 @@ def DrawCurve(x, y_list, std_list=[], xlabel='', ylabel='', title='', name_list=
         for index in range(len(y_list)):
             sub_y_list = y_list[index]
             sub_std_list = std_list[index]
-            if name_list[index] == CV_VAL:
+            if name_list[index] == LegendRename([CV_VAL][0]):
                 axes.errorbar(x, sub_y_list, yerr=sub_std_list, fmt='-o',
                               color=color_list[index], elinewidth=1, capsize=4,
                               alpha=1, label='CV Validation', marker='.')
