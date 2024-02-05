15943e26064640b9d2199536b91c2e02d927ed26
Suns <songyangmri@gmail.com>
2024-02-05 16:45:06 +0800

    Release 0.5.11

--- HomeUI/VersionConstant.py
+++ HomeUI/VersionConstant.py
@@ -7,7 +7,8 @@ All rights reserved.
 # Version Key
 MAJOR = 0
 MINOR = 5
-PATCH = 10
+PATCH = 11
 VERSION_NAME = 'Version'
 VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
-ACCEPT_VERSION = ['0.5.0', '0.5.1', '0.5.2', '0.5.3', '0.5.4', '0.5.5', '0.5.6', '0.5.7', '0.5.8', '0.5.9', '0.5.10']
+ACCEPT_VERSION = ['0.5.0', '0.5.1', '0.5.2', '0.5.3', '0.5.4', '0.5.5', '0.5.6', '0.5.7', '0.5.8', '0.5.9', '0.5.10',
+                  '0.5.11']
