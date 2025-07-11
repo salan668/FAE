91b0707055f582e06344931cc633f57d24a1eaef
SunsServer <songyangmri@gmail.com>
2025-07-11 21:56:35 +0800

    Release 0.6.3

--- HomeUI/VersionConstant.py
+++ HomeUI/VersionConstant.py
@@ -7,7 +7,7 @@ All rights reserved.
 # Version Key
 MAJOR = 0
 MINOR = 6
-PATCH = 2
+PATCH = 3
 VERSION_NAME = 'Version'
 VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
-ACCEPT_VERSION = ['0.6.0', '0.6.1', '0.6.2']
+ACCEPT_VERSION = ['0.6.0', '0.6.1', '0.6.2', '0.6.3']
